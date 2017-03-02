b035f8a0f070acb38fad42d9862c8ee9339ee8aa
tasuwo <kamuhata.you@gmail.com>
2017-03-02 22:04:50 +0900

    Initial commit

---
+++ flashcard/src/Controller/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  flashcard
+//
+//  Created by Tasuku Tozawa on 2017/03/01.
+//  Copyright © 2017 tasuku tozawa. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
